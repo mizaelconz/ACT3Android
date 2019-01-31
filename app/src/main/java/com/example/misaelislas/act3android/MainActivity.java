7e1adbc54dfc497d4d70c66f755e2a9b499bd2a4
mizaelconz <micho_11@hotmail.com>
2019-01-31 09:34:28 -0600

    Initial commit

---
+++ app/src/main/java/com/example/misaelislas/act3android/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.misaelislas.act3android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    Button button1;
+    Button button2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //finish();
+
+                Intent i = new Intent(MainActivity.this, Parte1.class);
+                startActivity(i);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //finish();
+
+                Intent i = new Intent(MainActivity.this, Parte2.class);
+                startActivity(i);
+            }
+        });
+    }
+}
+
+
